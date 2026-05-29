e382e92fc9f2d2cd8d6318d41957757e98575419
Stephen Williams <spwilliams2012@gmail.com>
2026-05-29 20:57:27 +0100

    Build update

--- a_build_all.bat
+++ a_build_all.bat
@@ -1,29 +1,44 @@
 :: Amok - perform a full build of program
+:: USE_8k_MEMORY_LAYOUT = 0 for unexpanded memory layout or, = 1 for 8K+ expanded memory layout
 @echo off
 
-:: Memory layout required to build
-:: 0 = unexpanded memory layout or 1 = 8K+ expanded memory layout
-set USE_8k_MEMORY_LAYOUT=1
-
-if "%USE_8k_MEMORY_LAYOUT%"=="1" (
-
-:: Build main program
-.\bin\acme.exe -l .\build\symbols -o .\build\amok -DUSE_8k_MEMORY_LAYOUT=1 .\main.asm
-
-:: Add the 2 load adddress bytes for the PRG header (PRG header created using Notepad++ with hex editor plugin)
-copy /b .\build\prgheader8k.bin+.\build\amok ".\prg\Amok.prg" >nul
-
-echo 8k version done!
-) else (
-
-:: Build main program
-.\bin\acme.exe -l .\build\symbols -o .\build\amok -DUSE_8k_MEMORY_LAYOUT=0 .\main.asm
-
-:: Add the 2 load adddress bytes for the PRG header (PRG header created using Notepad++ with hex editor plugin)
-copy /b .\build\prgheader.bin+.\build\amok ".\prg\Amok.prg" >nul
+::-----------------------------------------------------------------------------------
+set USE_8k_MEMORY_LAYOUT=0
+set "PRG=amok.prg"
+set "PRGHDR=prgheader.bin"
+call :create_prg_file_for_version
 
 :: Binary file comparison for unexpanded version
-fc.exe /b ".\prg\Amok.prg" ".\prg\Amok_original.prg"
+fc.exe /b ".\prg\%PRG%" ".\prg\Amok original.prg"
+echo --- Unexpanded version built ---
 
-echo Unexpanded version done!
-)
+::-----------------------------------------------------------------------------------
+set USE_8k_MEMORY_LAYOUT=1
+set "PRG=amok 8k.prg"
+set "PRGHDR=prgheader8k.bin"
+call :create_prg_file_for_version
+
+:: Binary file comparison for tested 8k version
+fc.exe /b ".\prg\%PRG%" ".\prg\amok 8k tested.prg"
+echo --- 8k version built ---
+
+goto :build_d64
+
+::-----------------------------------------------------------------------------------
+:: Subroutine to compile and copy renamed file into the prg folder
+:create_prg_file_for_version
+
+:: Compile main programs
+.\bin\acme.exe -l .\build\symbols -o .\build\main -DUSE_8k_MEMORY_LAYOUT=%USE_8k_MEMORY_LAYOUT% .\main.asm
+
+:: Add the 2 load address bytes for the PRG header (PRG header created using Notepad++ with hex editor plugin)
+copy /b .\build\%PRGHDR%+.\build\main ".\prg\%PRG%" >nul
+
+exit /B
+
+::-----------------------------------------------------------------------------------
+:: Subroutine to build the d64 file with both the unexpanded and 8k versions included
+:build_d64
+del ".\d64\Amok.d64"
+%PYPATH%\python .\utilities\create_d64.py
+echo Done!
